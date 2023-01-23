e1bcd801a0370d5fe9d66b462e2f563038351fba
ys <ysdeveloper01@gmail.com>
2023-01-23 16:13:45 +0800

    added shake icon effect

--- src/components/ImgButton.vue
+++ src/components/ImgButton.vue
@@ -23,7 +23,9 @@
     box-shadow: 0 0 0 4px #fff;
   }
 
-  img:hover {
+
+  img:active {
+    animation: shake 0.5s;
     opacity: 1;
   }
 
@@ -31,6 +33,53 @@
     img {
       height: 5vh;
       width: 12vw;
+    }
+  }
+
+
+  @keyframes shake {
+    0% {
+      transform: translate(1px, 1px) rotate(0deg);
+    }
+
+    10% {
+      transform: translate(-1px, -2px) rotate(-1deg);
+    }
+
+    20% {
+      transform: translate(-3px, 0px) rotate(1deg);
+    }
+
+    30% {
+      transform: translate(3px, 2px) rotate(0deg);
+    }
+
+    40% {
+      transform: translate(1px, -1px) rotate(1deg);
+    }
+
+    50% {
+      transform: translate(-1px, 2px) rotate(-1deg);
+    }
+
+    60% {
+      transform: translate(-3px, 1px) rotate(0deg);
+    }
+
+    70% {
+      transform: translate(3px, 1px) rotate(-1deg);
+    }
+
+    80% {
+      transform: translate(-1px, -1px) rotate(1deg);
+    }
+
+    90% {
+      transform: translate(1px, 2px) rotate(0deg);
+    }
+
+    100% {
+      transform: translate(1px, -2px) rotate(-1deg);
     }
   }
 </style>
